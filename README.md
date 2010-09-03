99c4ec0f8d54a9111bd48a7241173c3178a387f7
Aaron Heckmann <aaron.heckmann+github@gmail.com>
2010-09-02 22:54:59 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Node-Gameroom
+
+Everthing you need to get started creating realtime web-based games with nodejs.
+
+Includes
+* Express for handling your sites content
+* Socket.io for mananging your games realtime events and data
+* oAuth to let your users sign in easily
+* jQuery
+
+Excludes
+* Any database support. The choice is yours.
---
+++ app.js
@@ -0,0 +1,43 @@
+
+require.paths.unshift(
+  __dirname + '/support/express/support/connect/lib/'
+, __dirname + '/support/express/support/jade/lib/'
+, __dirname + '/support/redis-node-client/lib'
+, __dirname + '/support/connect-auth/lib'
+, __dirname + '/support/node-oauth/lib'
+, __dirname + '/support/node-formidable/lib/formidable/'
+)
+
+var express = require('./support/express')
+
+var app = module.exports = express.createServer(
+  express.errorHandler({ dumpExceptions: true, showStack: true})
+, express.conditionalGet()
+, express.logger()
+, express.cookieDecoder()
+, express.bodyDecoder()
+, express.staticProvider(__dirname + '/public')
+)
+
+app.configure(function(){
+  app.set('views', __dirname + '/views')
+})
+.error(function(err, req, res, next){
+  console.dir(err)
+  if (!err || 2 !== err.errno)
+    return res.render("500.jade", function(err, content){
+      res.send(content || "uhh, Look over there!", 500)  
+    })
+  res.render("404.jade", { layout: false }, function(err, content){
+    res.send(content || "aaaaaah!! I've been shot!", 404)  
+  })
+})
+
+require("./lib/routes")
+require("./lib/sockets")
+
+// never crash
+process.on("uncaughtException", function(err){
+  console.warn("Caught unhandled exception:")
+  console.warn(err.stack || err)    
+})
