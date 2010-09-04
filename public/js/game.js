f04bd80f3e735abf82525d11f885b7ca4f518dca
Aaron Heckmann <aaron.heckmann+github@gmail.com>
2010-09-04 14:23:23 -0400

    auto encode/decode and emit clientside events

--- public/js/game.js
+++ public/js/game.js
@@ -1,7 +1,43 @@
 ;(function($, io, toString){
 
   io.setPath("/js/socketio/")
+  
+  var global = window
+    , room = global.room = {}
 
+  room.doc = $(document)
+
+  var socket = room.socket = new io.Socket(location.hostname)
+  socket.connect()
+  socket.on("message", function(message){
+    //console.log(message)
+    // we don't use JSON.parsing, it's too slow
+    // for realtime solutions
+    // Emit all sent events on $(document)
+    var msg = message.split("|")
+      , doc = room.doc
+      , tok
+    while (tok = msg.shift()){
+      tok = tok.split("#")
+      doc.trigger(tok[0], tok.length > 1 ? tok[1].split(",") : [])
+    }
+  })
+
+  // skip json.stringify and use custom format
+  // more restricted but faster. IMO, speed wins the
+  // debate for realtime solutions
+  room.send = function(events){
+    // { event: [array of args], event: [array of args] }
+    // "EVENT_NAME#arg1,arg2,arg3,..|EVENT_NAME|..."
+    if (!events) return
+    var encoded = ""
+      , event
+    for (event in events){
+      encoded += event + "#" + events[event] + "|"
+    }
+    socket.send(encoded)
+  }
+ 
   // the flash transport whines if sending msgs too fast
   // but we seem to be able to safely ignore those
   if (console && "function" == typeof console.error){
@@ -12,32 +48,10 @@
       __err(err)
     }
   }
-  
-  var global = window
-    , room = global.room = {}
-
-  room.doc = $(document)
-
-  var socket = room.socket = new io.Socket(location.hostname)
-  socket.connect()
-  socket.on("message", function(message){
-    var decoded = room.decode ? room.decode(message) : message
-    // devs can return false from room.decode to specify
-    // that they handled everything so we don't have to
-    if (false === decoded) return
-    room.doc.trigger("message", decoded)
-  })
 
   // cross browser happiness
   room.log = console && "function" == typeof console.log
     ? function(){ console.log.apply(console, arguments) }
     : function(){}
-
-  room.send = function(events){
-    // room.encode allows devs to specify their own encoding
-    events = room.encode ? room.encode(events) : events
-    socket.send(events)
-  }
-  
 
 })(jQuery, io, Object.prototype.toString)
